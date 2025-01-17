3f7d085940a9a5c1e0ab906f7aad66e04a246eb9
ppabam <data.mario24@gmail.com>
2025-01-18 04:29:04 +0900

    로그인 후 admin 페이지로 주소창 갱신후에야 로그아웃 되는 버그 있음

--- auth.config.ts
+++ auth.config.ts
@@ -9,13 +9,32 @@ export const authConfig = {
     // while this file is also used in non-Node.js environments
   ],
   callbacks: {
+    // async redirect({ url, baseUrl }) {
+    //   console.log('redirect', url, baseUrl);
+    //   // // 로그인 후 /admin 페이지로 리다이렉트
+    //   // if (url === baseUrl || url === '/') {
+    //   //   return `${baseUrl}`; // 로그인 후 /admin으로 리다이렉트
+    //   // }
+
+    //   // 로그인 페이지로 가는 루프 방지
+    //   if (url === `${baseUrl}/login`) {
+    //     return `${baseUrl}/admin`; // 로그인 페이지에서 리다이렉트되지 않도록 처리
+    //   }
+    //   return url;
+    // },
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnAdmin = nextUrl.pathname.startsWith('/admin');
+      const isOnLogin = nextUrl.pathname.startsWith('/login');
 
       if (isOnAdmin && !isLoggedIn) {
         // ✅ 비로그인 상태에서 /admin 접근 시 로그인 페이지로 리다이렉트
         return Response.redirect(new URL('/404', nextUrl));
+      }
+
+      if (isOnLogin && isLoggedIn) {
+        // ✅ 로그인 상태에서 /login 접근 시 /admin으로 리다이렉트
+        return Response.redirect(new URL('/admin', nextUrl));
       }
 
       return true;
