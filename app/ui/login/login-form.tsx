5ff3bd29832f861a86a8c8da2e4fff831c8ad4d8
ppabam <data.mario24@gmail.com>
2025-01-17 22:46:12 +0900

    Type error: Could not find a declaration file for module 'bcrypt'. '/home/ppabam/code/flag123/node_modules/bcrypt/bcrypt.js' implicitly has an 'any' type

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { authenticate } from '@/app/lib/actions';
+import { authenticate } from '@/app/lib/action';
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
