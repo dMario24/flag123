416f7c2ebed0417ba62e9d247d7b0a612ed02117
ppabam <data.mario24@gmail.com>
2025-01-19 06:16:53 +0900

    Response.redirect -> NextResponse.redirect
    - https://chatgpt.com/share/678c1a4d-86f4-8008-8407-7145964ca662

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
+import { NextResponse } from "next/server";
 
 export const authConfig = {
   pages: {
@@ -9,37 +10,64 @@ export const authConfig = {
     // while this file is also used in non-Node.js environments
   ],
   callbacks: {
-    // async redirect({ url, baseUrl }) {
-    //   console.log('redirect', url, baseUrl);
-    //   // // 로그인 후 /admin 페이지로 리다이렉트
-    //   // if (url === baseUrl || url === '/') {
-    //   //   return `${baseUrl}`; // 로그인 후 /admin으로 리다이렉트
-    //   // }
-
-    //   // 로그인 페이지로 가는 루프 방지
-    //   if (url === `${baseUrl}/login`) {
-    //     return `${baseUrl}/admin`; // 로그인 페이지에서 리다이렉트되지 않도록 처리
-    //   }
-    //   return url;
-    // },
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnAdmin = nextUrl.pathname.startsWith('/admin');
       const isOnLogin = nextUrl.pathname.startsWith('/login');
 
-      if (!isLoggedIn && isOnAdmin) {
-        // ✅ 비로그인 상태에서 /admin 접근 시 로그인 페이지로 리다이렉트
-        console.log("✅ 비로그인 상태에서 /admin 접근 시 로그인 페이지로 리다이렉트");
-        return Response.redirect(new URL('/404', nextUrl));
-      }
+      // if (!isLoggedIn && isOnAdmin) {
+      //   // ✅ 비로그인 상태에서 /admin 접근 시 로그인 페이지로 리다이렉트
+      //   console.log("✅ 비로그인 상태에서 /admin 접근 시 로그인 페이지로 리다이렉트");
+      //   return Response.redirect(new URL('/404', nextUrl));
+      // }
 
       if (isLoggedIn && isOnLogin) {
         // ✅ 로그인 상태에서 /login 접근 시 /admin으로 리다이렉트
         console.log("✅ 로그인 상태에서 /login 접근 시 /admin으로 리다이렉트");
-        return Response.redirect(new URL('/admin', nextUrl));
+        // return Response.redirect(new URL('/admin', nextUrl));
+        return NextResponse.redirect(new URL('/admin', nextUrl));
       }
       console.log("authorized return true");
       return true;
     },
   },
 } satisfies NextAuthConfig;
+
+
+// export async function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+
+//   if (pathname.startsWith("/api/flags")) {
+//     const headerKey = request.headers.get("Authorization")?.split(" ")[1];
+//     const queryKey = request.nextUrl.searchParams.get("k123");
+
+//     const apiKey = process.env.NEXT_PUBLIC_F123_API_KEY || "날리면";
+//     if (headerKey === apiKey || queryKey === apiKey) {
+//       return NextResponse.next(); // 인증 성공
+//     }
+//     return NextResponse.redirect(`${process.env.BASE_URL}/403`); // 인증 실패
+//   }
+
+//   const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
+
+//   // /admin 경로에 대한 인증 처리
+//   if (pathname.startsWith('/admin')) {
+//     if (!token) {
+//       // 인증되지 않은 사용자는 /login 페이지로 리다이렉트
+//       return NextResponse.redirect(new URL('/login', request.url));
+//     }
+//   }
+
+
+//   // /login 경로에 대한 인증후 admin 이동 처리
+//   if (pathname.startsWith('/login')) {
+//     const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
+//     if (token) {
+//       // 인증된 사용자는 /admin 페이지로 리다이렉트
+//       return NextResponse.redirect(new URL('/admin', request.url));
+//     }
+//   }
+
+//   // For all other paths, continue without interruption
+//   return NextResponse.next();
+// }
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
- 
+
 export default NextAuth(authConfig).auth;
+import { getToken } from 'next-auth/jwt';
 
 // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
@@ -12,18 +13,46 @@ export const config = {
     // "/api/flags",
     // "/api/flags/imgbb",
     // "/api/flags/insert",
+    // "/admin",
     '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+    // '/((?!_next/static|_next/image|.*\\.png$).*)',
   ], // 이 경로들에만 미들웨어가 적용됩니다.
 };
 
-// export async function middleware(request: NextRequest) {
-//   const headerKey = request.headers.get("Authorization")?.split(" ")[1];
-//   const queryKey = request.nextUrl.searchParams.get("k123");
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
 
-//   const apiKey = process.env.NEXT_PUBLIC_F123_API_KEY || "날리면";
-//   if (headerKey === apiKey || queryKey === apiKey) {
-//     return NextResponse.next(); // 인증 성공
-//   }
+  if (pathname.startsWith("/api/flags")) {
+    const headerKey = request.headers.get("Authorization")?.split(" ")[1];
+    const queryKey = request.nextUrl.searchParams.get("k123");
 
-//   return NextResponse.redirect(`${process.env.BASE_URL}/403`); // 인증 실패
-// }
+    const apiKey = process.env.NEXT_PUBLIC_F123_API_KEY || "날리면";
+    if (headerKey === apiKey || queryKey === apiKey) {
+      return NextResponse.next(); // 인증 성공
+    }
+    return NextResponse.redirect(`${process.env.BASE_URL}/403`); // 인증 실패
+  }
+
+  const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
+
+  // /admin 경로에 대한 인증 처리
+  if (pathname.startsWith('/admin')) {
+    if (!token) {
+      // 인증되지 않은 사용자는 /login 페이지로 리다이렉트
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
+
+  // /login 경로에 대한 인증후 admin 이동 처리
+  if (pathname.startsWith('/login')) {
+    const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
+    if (token) {
+      // 인증된 사용자는 /admin 페이지로 리다이렉트
+      return NextResponse.redirect(new URL('/admin', request.url));
+    }
+  }
+
+  // For all other paths, continue without interruption
+  return NextResponse.next();
+}
