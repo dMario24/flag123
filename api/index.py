29c87538f22ea67a1d693251eec1602c4181e39e
dMario24 <data.mario24@gmail.com>
2025-02-02 17:43:57 +0900

    형태소 분석 API 추가
    ```
    curl -X 'GET' \
      'http://localhost:3000/api/py/n/%EC%95%84%EB%B2%84%EC%A7%80%EA%B0%80%EB%B0%A9%EC%97%90%EB%93%A4%EC%96%B4%EA%B0%80%EC%8B%A0%EB%8B%A4' \
      -H 'accept: application/json'
    ```

--- api/index.py
+++ api/index.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from datetime import datetime, date
 from typing import Dict
-import random
 from flag_morph.extract import extract_nouns
 
 ### Create FastAPI instance with custom docs and openapi url
@@ -10,4 +8,9 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/hipy")
 def hello_fast_api():
     return {"message": "HI PYTHON FastAPI"}
+
+@app.get("/api/py/n/{sentence}")
+def get_nouns(sentence: str) -> Dict[str, list[str]]:
+    r = extract_nouns(sentence)
+    return {"nouns": r}
 
